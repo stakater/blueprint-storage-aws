20a6b110ab9eb8c56e1330460ad730a842ea5ac9
Hazim <hazim@aurorasolutions.io>
2016-09-30 12:51:17 +0500

    Update output var name

--- modules/efs/mount-target/output.tf
+++ modules/efs/mount-target/output.tf
@@ -2,6 +2,6 @@ output "dns-names" {
   value = "${join(",", aws_efs_mount_target.efs-mount-target.*.dns_name)}"
 }
 
-output "id" {
+output "ids" {
   value = "${join(",", aws_efs_mount_target.efs-mount-target.*.id)}"
 }
