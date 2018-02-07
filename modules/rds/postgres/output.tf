afadb0ebecc595e19f796c04381f1f05719a2103
asim <asimawan.42@gmail.com>
2018-02-07 16:24:28 +0500

    Remove sg id output var

--- modules/rds/postgres/output.tf
+++ modules/rds/postgres/output.tf
@@ -1,12 +1,3 @@
 output "endpoint" {
   value = "${aws_db_instance.postgresql.endpoint}"
 }
-
-# To allow user to add custom rules to the security group
-output "security-group-id"{
-  value = "${
-    length(var.allowed_security_groups) == 0
-     ? element(aws_security_group.postgresql_vpc.*.id, 0)
-     : element(aws_security_group.postgresql_sg.*.id, 0)
-  }"
-}
