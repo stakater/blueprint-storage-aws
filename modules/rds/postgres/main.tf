889ed2c69686158c9e8fd0d99595589dbf85ef0d
asim <asimawan.42@gmail.com>
2018-02-08 14:55:57 +0500

    Fix reference name for explicit dependency

--- modules/rds/postgres/main.tf
+++ modules/rds/postgres/main.tf
@@ -22,7 +22,7 @@ resource "aws_db_instance" "postgresql" {
   multi_az                   = "${var.multi_availability_zone}"
   port                       = "${var.database_port}"
   vpc_security_group_ids     = ["${element(concat(aws_security_group.postgresql_vpc.*.id, aws_security_group.postgresql_sg.*.id), 0)}"]
-  db_subnet_group_name       = "${var.name}-subnet-group"
+  db_subnet_group_name       = "${aws_db_subnet_group.postgresql_db_subnet_group.name}"
   parameter_group_name       = "${var.name}-parameter-group"
   storage_encrypted          = "${var.storage_encrypted}"
 
