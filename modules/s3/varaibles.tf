c297065ae6937be8e98d626db9e22ee9613e2ccc
ahsan-storm <ahsanmuhammad1@outlook.com>
2018-10-31 18:42:35 +0500

    adds resource for creating public s3 bucket for static website hosting

--- modules/s3/varaibles.tf
+++ modules/s3/varaibles.tf
@@ -31,3 +31,23 @@
 variable "name" {
   default = "Name of the bucket"
 }
+
+variable "acl" {
+  default = "private"
+}
+
+variable "policy" {
+  default = ""
+}
+
+variable "index_document" {
+  default = "index.html"
+}
+
+variable "error_document" {
+  default = ""
+}
+
+variable "routing_rules" {
+  default = ""
+}
