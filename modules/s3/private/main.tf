91a614a6f8e3594b70cf64f39625b6ce54ff6d78
Muhammad Hamza Zaib <hamza@aurorasolutions.io>
2019-03-19 13:36:42 +0500

    Add aws_s3_bucket_public_access_block

--- modules/s3/private/main.tf
+++ modules/s3/private/main.tf
@@ -36,3 +36,12 @@ resource "aws_s3_bucket" "s3_bucket_private" {
         Name = "${var.name}"
     }
 }
+
+resource "aws_s3_bucket_public_access_block" "s3_bucket_private_access_block" {
+  bucket = "${aws_s3_bucket.s3_bucket_private.id}"
+
+  block_public_acls   = "${var.block_public_acls}"
+  block_public_policy = "${var.block_public_policy}"
+  ignore_public_acls  = "${var.ignore_public_acls}"
+  restrict_public_buckets = "${var.restrict_public_buckets}"
+}
--- modules/s3/private/varaibles.tf
+++ modules/s3/private/varaibles.tf
@@ -35,3 +35,19 @@ variable "name" {
 variable "acl" {
   default = "private"
 }
+
+variable "block_public_acls" {
+  default = true
+}
+
+variable "block_public_policy" {
+  default = true
+}
+
+variable "ignore_public_acls" {
+  default = true
+}
+
+variable "restrict_public_buckets" {
+  default = true
+}
